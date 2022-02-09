cd4fa0a446e34ea1c386ec24fbfc093ffaee209f
April Dockery <44369984+dockeryapril@users.noreply.github.com>
2022-02-09 12:06:57 -0500

    commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Select all the elements in the HTML page
 // and assign them to a variable
-let now_playing = document.querySelector('.now-playing');
+let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector(".track-name");
 let track_artist = document.querySelector(".track-artist");
